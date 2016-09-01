c4a8d206828d9bb3eee0bf810f59249c0b4c574a
mikat <mika2000.10@gmail.com>
2016-09-01 15:47:48 +0200

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+
+/* POST method for getting the descs */
+router.post('/data', function(req, res, next) {
+  desclist = []
+  req.body.urls.forEach(function(word, ind){
+    url = "http://www.woorden.org/woord/" + word
+    request(url, function(error, response, html){
+      if(!error){
+        var $ = cheerio.load(html);
+        var desc
+        $('.span8').filter(function(){
+          var data = $(this);
+          desc = data.eq(2).eq(0).index('font').eq(0).text();
+          desclist.push(desc);
+        })
+      }
+    })
+  });
+  res.send(desclist);
+});
+
+module.exports = router;
